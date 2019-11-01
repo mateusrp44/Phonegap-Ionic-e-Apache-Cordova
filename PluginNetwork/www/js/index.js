02cf48628608ba0ff6caeaa64f25368775dcd3b2
Mateus Rocha Pinto <mateusrpinto44@gmail.com>
2019-11-01 15:46:54 -0300

    PluginNetwork (corrected)

--- PluginNetwork/www/js/index.js
+++ PluginNetwork/www/js/index.js
@@ -33,17 +33,17 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        var networkState = navigator.connection.type;
+        var states = {};
+        states[Connection.UNKNOWN]  = 'Conexão desconhecida';
+        states[Connection.ETHERNET] = 'Conexão Ethernet';
+        states[Connection.WIFI]     = 'Conexão WiFi';
+        states[Connection.CELL_2G]  = 'Conexão 2G';
+        states[Connection.CELL_3G]  = 'Conexão 3G';
+        states[Connection.CELL_4G]  = 'Comexão 4G';
+        states[Connection.CELL]     = 'conexão genérica';
+        states[Connection.NONE]     = 'Sem conexão de rede';
+        //alert('Connection type: ' + states[networkState]);
+        document.getElementById("connectionType").innerHTML = states[networkState];
     }
 };
