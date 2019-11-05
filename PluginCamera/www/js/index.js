ce0e1434e6bbe41e2ff05f285a7dc84a89fc1ce5
Mateus Rocha Pinto <mateusrpinto44@gmail.com>
2019-11-05 08:38:54 -0300

    PluginCamera

---
+++ PluginCamera/www/js/index.js
@@ -0,0 +1,32 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        //document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    
+    capturePhoto: function(){
+
+        navigator.camera.getPicture(onSuccess, onFail, { 
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL,
+            saveToPhotoAlbum: true 
+        });
+
+        function onSuccess(imageData) {
+            var image = document.getElementById('minhaImagem');
+            image.style.display = "block";
+            image.src = "data:image/jpeg;base64," + imageData;
+        }
+
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        }
+    }
+};
